e1d20bc91eed03af3ef946d8b2b2e0e54e69518b
DAN LUIGI TATING (SHP-IRIS-ISD-OOCLL/MNL) <TATINDA@OOCL.COM>
2019-10-16 15:48:16 +0800

    [TATINDA/Woody]-  Added Patch Method

--- src/main/java/com/tw/apistackbase/controller/CompanyController.java
+++ src/main/java/com/tw/apistackbase/controller/CompanyController.java
@@ -2,8 +2,11 @@ package com.tw.apistackbase.controller;
 
 import com.tw.apistackbase.core.Company;
 import com.tw.apistackbase.repository.CompanyRepository;
+import javassist.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/companies")
@@ -20,5 +23,16 @@ public class CompanyController {
     @PostMapping(produces = {"application/json"})
     public Company add(@RequestBody Company company) {
         return companyRepository.save(company);
+    }
+
+    @PatchMapping(value = "/{id}", produces = {"application/json"})
+    public Company update(@PathVariable Long id, @RequestBody Company company) throws NotFoundException {
+        Optional<Company> fetchedCompany = companyRepository.findById(id);
+        if (fetchedCompany.isPresent()) {
+            Company modifyCompany = fetchedCompany.get();
+            modifyCompany.setName(company.getName());
+            return companyRepository.save(modifyCompany);
+        }
+        throw new NotFoundException("Company id not found");
     }
 }
