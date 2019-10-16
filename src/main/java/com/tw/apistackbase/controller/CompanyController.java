64b4f891bbe68c2927ed75142b8320180bd6d5c7
DAN LUIGI TATING (SHP-IRIS-ISD-OOCLL/MNL) <TATINDA@OOCL.COM>
2019-10-16 15:56:51 +0800

    [TATINDA/Woody]-  Refactor

--- src/main/java/com/tw/apistackbase/controller/CompanyController.java
+++ src/main/java/com/tw/apistackbase/controller/CompanyController.java
@@ -4,6 +4,8 @@ import com.tw.apistackbase.core.Company;
 import com.tw.apistackbase.repository.CompanyRepository;
 import javassist.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
@@ -21,18 +23,20 @@ public class CompanyController {
     }
 
     @PostMapping(produces = {"application/json"})
+    @ResponseStatus(code = HttpStatus.CREATED)
     public Company add(@RequestBody Company company) {
         return companyRepository.save(company);
     }
 
     @PatchMapping(value = "/{id}", produces = {"application/json"})
-    public Company update(@PathVariable Long id, @RequestBody Company company) throws NotFoundException {
+    public ResponseEntity<Company> update(@PathVariable Long id, @RequestBody Company company) {
         Optional<Company> fetchedCompany = companyRepository.findById(id);
         if (fetchedCompany.isPresent()) {
             Company modifyCompany = fetchedCompany.get();
             modifyCompany.setName(company.getName());
-            return companyRepository.save(modifyCompany);
+            Company savedCompany = companyRepository.save(modifyCompany);
+            return new ResponseEntity<>(savedCompany, HttpStatus.OK);
         }
-        throw new NotFoundException("Company id not found");
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 }
