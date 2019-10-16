89172d5828adbb108ff86c122c8b59923365b86f
DAN LUIGI TATING (SHP-IRIS-ISD-OOCLL/MNL) <TATINDA@OOCL.COM>
2019-10-16 15:10:32 +0800

    [TATINDA/Woody]-  Added One to One

--- src/main/java/com/tw/apistackbase/core/Company.java
+++ src/main/java/com/tw/apistackbase/core/Company.java
@@ -1,9 +1,6 @@
 package com.tw.apistackbase.core;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 public class Company {
@@ -11,6 +8,17 @@ public class Company {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String name;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    private CompanyProfile profile;
+
+    public CompanyProfile getProfile() {
+        return profile;
+    }
+
+    public void setProfile(CompanyProfile profile) {
+        this.profile = profile;
+    }
 
     public Long getId() {
         return id;
